dd4157845822442bfeaeaec0982841294b48a3c9
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-23 11:58:03 +0300

    add trpc

---
+++ src/pages/api/auth/[trpc].ts
@@ -0,0 +1,26 @@
+import { initTRPC } from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+import { z } from 'zod';
+
+export const t =  initTRPC.create();
+
+export const appRouter = t.router({
+  hello: t.procedure.input(
+    z.object({
+      text: z.string(),
+    })
+  ).query(({ input }) => {
+      return {
+        greeting: `hello ${input.text ?? 'world'}`,
+      };
+    }),
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+// export API handler
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => ({}),
+});
