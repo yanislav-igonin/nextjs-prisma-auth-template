84055a18c7c85227417c87c4aa1607313dc547d2
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-23 14:24:13 +0300

    update paths in tsconfig

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -1,7 +1,7 @@
 import { createTRPCNext } from '@trpc/next';
 import { httpBatchLink, loggerLink } from '@trpc/client';
 import superjson from 'superjson';
-import type { AppRouter } from '../pages/api/trpc/[trpc]';
+import type { AppRouter } from '@pages/api/trpc/[trpc]';
 
 function getBaseUrl() {
   if (typeof window !== 'undefined') // browser should use relative path
