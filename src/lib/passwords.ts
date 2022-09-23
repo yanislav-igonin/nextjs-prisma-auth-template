3e3fd8e7f6a49d9aa1a6eb5123515b4a721c3720
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-23 22:25:24 +0300

    lint

--- src/lib/passwords.ts
+++ src/lib/passwords.ts
@@ -1,3 +1,4 @@
 import bcrypt from 'bcrypt';
 
-export const compare = async (password: string, hash: string) => bcrypt.compare(password, hash);
+export const compare = async (password: string, hash: string) => 
+  bcrypt.compare(password, hash);
