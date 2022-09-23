59c59147f144f54da2a9ee8040e4d52c027996d6
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-23 22:24:14 +0300

    disable refetch on focus

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -33,7 +33,7 @@ export const trpc = createTRPCNext<AppRouter>({
       /**
        * @link https://react-query-v3.tanstack.com/reference/QueryClient
        **/
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+      queryClientConfig: { defaultOptions: { queries: { refetchOnWindowFocus: false } } },
     };
   },
   /**
