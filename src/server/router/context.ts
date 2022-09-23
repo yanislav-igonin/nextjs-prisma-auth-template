f50b3e0436f0088470b9f489ae9d91cba23bc1c0
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-23 15:24:46 +0300

    add routers

---
+++ src/server/router/context.ts
@@ -0,0 +1,26 @@
+import * as trpc from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+import { db } from '@db/client';
+
+export const createContext = async (
+  opts?: trpcNext.CreateNextContextOptions,
+) => {
+  const req = opts?.req;
+  const res = opts?.res;
+
+  const sessionId = req?.cookies?.sid;
+
+  const session =
+    req && res && (await db.session.findFirst({ where: { id: sessionId } }));
+
+  return {
+    req,
+    res,
+    session,
+    prisma,
+  };
+};
+
+type Context = trpc.inferAsyncReturnType<typeof createContext>;
+
+export const createRouter = () => trpc.router<Context>();
