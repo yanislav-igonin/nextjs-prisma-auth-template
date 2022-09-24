523e8ac29709e9e6e128929c4377d801afceedb4
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-24 23:28:45 +0300

    add meta viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => <Html className="dark">
   <Head>
     <meta name="description" content="My NextJS Template" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
 
