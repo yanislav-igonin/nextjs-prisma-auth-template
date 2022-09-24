8de3b4e1ae57969b401bd93ce673b13589524eac
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-24 11:50:44 +0300

    add disabled state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,9 +3,14 @@ import { FC } from 'react';
 type Props = {
   text: string;
   onClick: () => void;
+  disabled?: boolean;
+  loading?: boolean;
 }
-export const Button: FC<Props> = ({ text, onClick }) => <button
+export const Button: FC<Props> = ({
+  text, onClick, disabled = false, loading = false,
+}) => <button
   onClick={onClick}
+  disabled={disabled || loading}
   className="bg-emerald-300 rounded-sm">
   {text}
 </button>;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,19 @@
 import { HTMLInputTypeAttribute, FC } from 'react';
 
 type Props = {
-  placeholder: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   value: string;
-  type: HTMLInputTypeAttribute;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  type?: HTMLInputTypeAttribute;
+  disabled?: boolean;
 }
-export const Input: FC<Props> = ({ placeholder, type, value, onChange }) => <input
-  type={type}
-  value={value}
-  onChange={onChange}
-  className="bg-slate-200 rounded-sm"
-  placeholder={placeholder}
-/>;
+export const Input: FC<Props> = ({
+  placeholder = '', type = 'text', value, onChange, disabled = false,
+}) => <input
+    value={value}
+    onChange={onChange}
+    type={type}
+    className="bg-slate-200 rounded-sm"
+    placeholder={placeholder}
+    disabled={disabled}
+  />;
