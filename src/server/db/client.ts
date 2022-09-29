cc64fb3a656cf0f5b41d792e8bdb8eeac372cc35
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-29 22:44:42 +0300

    update prisma export

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -6,8 +6,6 @@ declare global {
 }
 
 // Prevent multiple instances of Prisma Client in development
-const prisma = global.prisma || new PrismaClient({ log: ['query'] });
+export const db = global.prisma || new PrismaClient({ log: ['query'] });
 
-if (config.isProduction) global.prisma = prisma;
-
-export { prisma as db };
+if (config.isProduction) global.prisma = db;
