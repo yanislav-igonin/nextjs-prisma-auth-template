65a43dfbcd354a48295070090291ebe0db3a0efe
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-30 00:00:07 +0300

    add links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { DarkModeButton, MainCard } from '@components';
 import { db } from '@db';
 import { trpc } from '@lib/trpc';
+import Link from 'next/link';
 
 const loginRedirect = {
   redirect: {
@@ -40,11 +41,22 @@ const Home: NextPage = () => {
     <main className="flex flex-col h-screen justify-center items-center bg-gradient-to-tr from-emerald-400 to-fuchsia-400 dark:from-emerald-600 dark:to-fuchsia-600">
       {helloQuery.data
         ? <h1 className="text-2xl mb-2 text-center dark:text-white">
-            <>{helloQuery.data.message}</> at <>{helloQuery.data.time.toLocaleString()}</>
-          </h1>
+          <>{helloQuery.data.message}</> at <>{helloQuery.data.time.toLocaleString()}</>
+        </h1>
         : <h1 className="text-2xl mb-2 text-center dark:text-white">loading...</h1>}
 
       <h1 className='dark:text-white text-2xl mb-2'>email: {me}</h1>
+
+      <h1 className="text-2xl mb-2 dark:text-white">pages:</h1>
+
+      <div className='flex flex-row gap-2'>
+        <Link href="/users">
+          <a className="text-2xl dark:text-white">🤙 users</a>
+        </Link>
+        <Link href="/auth/login" className="text-xl dark:text-white">
+          <a className="text-2xl dark:text-white">🤙 login</a>
+        </Link>
+      </div>
 
       <MainCard />
     </main>
