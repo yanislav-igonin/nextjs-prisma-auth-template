2fdda5fc791df88ba6d78860b4f9539f1ed2e737
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-29 22:46:47 +0300

    protect getAll users api path

--- src/server/routers/users.router.ts
+++ src/server/routers/users.router.ts
@@ -1,7 +1,8 @@
+import { authMiddleware } from '@middlewares';
 import { t } from '@trpc-server';
 
 export const usersRouter = t.router({
-  getAll: t.procedure.query(async ({ ctx: { db } }) => {
+  getAll: t.procedure.use(authMiddleware).query(async ({ ctx: { db } }) => {
     const users = await db.user.findMany({
       select: { email: true, id: true },
     });
