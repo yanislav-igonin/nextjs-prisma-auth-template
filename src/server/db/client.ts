316604881b9e33f2e7e7825a901d1a461511d579
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-29 12:01:20 +0300

    add global declaration

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 import { config } from '@config';
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
 // Prevent multiple instances of Prisma Client in development
 const prisma = global.prisma || new PrismaClient({ log: ['query'] });
 
