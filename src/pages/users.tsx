a16ccc88fed19907087ab911a207122fdd70dfa8
Yanislav Igonin <yanislavdabest@gmail.com>
2022-09-29 23:27:09 +0300

    users page wip

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,7 +1,40 @@
+import { db } from '@db';
+import { User } from '@prisma/client';
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next';
 
+const loginRedirect = {
+  redirect: {
+    destination: '/auth/login',
+    permanent: false,
+  },
+};
 
-const Users = () => {
-  return <div>users</div>;
+type ServerSideProps = {
+  users: Pick<User, 'id' | 'email'>[];
+};
+
+export const getServerSideProps: GetServerSideProps<ServerSideProps> = async ({ req }) => {
+  const sessionId = req.cookies.sid || '';
+  if (!sessionId) {
+    return loginRedirect;
+  }
+  const session = await db.session.findFirst({ where: { id: sessionId } });
+  if (!session || session.expires < new Date()) {
+    return loginRedirect;
+  }
+  const users = await db.user.findMany({ select: { email: true, id: true } });
+  return {
+    props: { users },
+  };
+};
+
+const Users: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ users }) => {
+  return <div>
+    <h1>Users</h1>
+    <ul>
+      {users.map((user) => <li key={user.id}>{user.email}</li>)}
+    </ul>
+  </div>;
 };
 
 export default Users;
