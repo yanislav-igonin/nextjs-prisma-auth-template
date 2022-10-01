e0a0af0c8583f3fde28b2a85512303bb7c018eac
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-01 16:10:17 +0300

    add sidebar menu

---
+++ src/components/SidebarMenu.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import { HomeIcon, UsersIcon } from '@heroicons/react/20/solid';
+
+type Link = {
+  href: string;
+  label: string;
+  icon: JSX.Element;
+}
+
+export const SidebarMenu = () => {
+  const links: Link[] = [
+    { href: '/', label: 'Home', icon: <HomeIcon /> },
+    { href: '/users', label: 'Users', icon: <UsersIcon /> },
+    { href: '/users-ssr', label: 'Users SSR', icon: <UsersIcon /> },
+  ];
+
+  return <nav className='flex flex-col gap-2 absolute'>
+    {links.map((link) => <MenuLink key={link.href} {...link} />)}
+  </nav>;
+};
+
+const MenuLink = ({ href, label, icon }: Link) =>
+  <Link href={href} key={href}>
+    <a className='text-lg cursor-pointer h-8 w-8'>
+      {icon}
+    </a>
+  </Link>;
