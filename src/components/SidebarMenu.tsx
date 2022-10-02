c9e1c7977706d9e6f3c5403e81ef96e8edcd3c5e
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-02 11:26:59 +0300

    add active menu styling

--- src/components/SidebarMenu.tsx
+++ src/components/SidebarMenu.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { HomeIcon, UsersIcon } from '@heroicons/react/20/solid';
+import { useRouter } from 'next/router';
 
 type Link = {
   href: string;
@@ -13,15 +14,22 @@ export const SidebarMenu = () => {
     { href: '/users', label: 'Users', icon: <UsersIcon /> },
     { href: '/users-ssr', label: 'Users SSR', icon: <UsersIcon /> },
   ];
+  const router = useRouter();
+  const currentPath = router.pathname;
 
   return <nav className='flex flex-col gap-2 h-screen'>
-    {links.map((link) => <MenuLink key={link.href} {...link} />)}
+    {links.map((link) =>
+      <MenuLink key={link.href} {...link}
+        isActive={link.href === currentPath} />
+    )}
   </nav>;
 };
 
-const MenuLink = ({ href, label, icon }: Link) =>
+type MenuLinkProps = Link & { isActive: boolean };
+
+const MenuLink = ({ href, label, icon, isActive }: MenuLinkProps) =>
   <Link href={href} key={href}>
-    <a className='text-lg cursor-pointer h-8 w-8'>
+    <a className={`text-lg cursor-pointer w-10 ${isActive ? 'bg-rose-500' : ''}`}>
       {icon}
     </a>
   </Link>;
