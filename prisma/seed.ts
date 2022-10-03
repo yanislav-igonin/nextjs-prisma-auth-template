314d9e834fca3258405a702fe2f1b2858622b160
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-03 16:29:08 +0300

    optimize users seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,20 +7,11 @@ const usersSeedCount = 1000;
 
 const main = async () => {
   const password = await bcrypt.hash('1234qwerA_', HASH_ROUNDS);
-  await prisma.user.create({
-    data: {
-      email: 'admin@admin.com',
-      password,
-    },
-  });
+  const users = [{ email: 'admin@admin.com', password }];
   for (let i = 0; i < usersSeedCount; i++) {
-    await prisma.user.create({
-      data: {
-        email: `seed${i}@user.com`,
-        password,
-      },
-    });
+    users.push({ email: `seed${i}@user.com`, password });
   }
+  await prisma.user.createMany({ data: users });
 };
 
 main()
