5e7c55c79e6f0c70edf24769b67668d54e2ed742
Yanislav Igonin <yanislavdabest@gmail.com>
2022-11-18 15:54:37 +0200

    styling

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -25,9 +25,9 @@ const Login: NextPage = () => {
 
   return <div className='flex items-center justify-center h-screen'>
     <form onSubmit={onSubmit} className='flex flex-col w-full px-4 md:w-1/4'>
-      <h1 className='text-2xl mb-0 text-center'>login:</h1>
+      <h1 className='text-2xl mb-0 font-bold text-center'>login</h1>
       <h1 className='text-2xl mb-0 text-red-500 text-center'>admin@admin.com</h1>
-      <h1 className='text-2xl mb-0 text-center'>password:</h1>
+      <h1 className='text-2xl mb-0 font-bold text-center'>password</h1>
       <h1 className='text-2xl mb-2 text-red-500 text-center'>1234qwerA_</h1>
       <div className='m-1'>
         <Input placeholder='Email' disabled={login.isLoading} type="email" value={email} onChange={e => setEmail(e.target.value)} />
